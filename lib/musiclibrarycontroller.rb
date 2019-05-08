868bdfc5f7fb5839f553399bbbae593e4954552c
Gregory Losco <gtlosco@gmail.com>
2019-05-08 02:35:32 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -55,7 +55,7 @@ class MusicLibraryController
     input = gets.strip
     if list_songs.include?(input)
       song = list_songs(input)
-      puts "Playing #{song.name}"
+      puts "Playing #{song.name} by #{song.artist}"
   end
   
 end
