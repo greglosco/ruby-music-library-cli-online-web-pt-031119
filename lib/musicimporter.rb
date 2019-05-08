9c2c8288bd511c4057640dc7f045cb336dd62e42
Gregory Losco <gtlosco@gmail.com>
2019-05-08 01:43:14 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -6,7 +6,7 @@ class MusicImporter
   end
   
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect {|file| file.gsub}
+    @files ||= Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}")}
   end
   
 end
