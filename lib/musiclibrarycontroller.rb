5b22665b75b2473a7080186f98ea8b9dfaefdb0a
Gregory Losco <gtlosco@gmail.com>
2019-05-08 02:47:56 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,6 +23,11 @@ class MusicLibraryController
       when "list songs"
         list_songs
       end
+      
+      case input
+      when "list artists"
+        list_artists
+      end
     
     
     end
