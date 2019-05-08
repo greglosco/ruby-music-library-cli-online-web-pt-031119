51e325f54541abf37a93c3fbc2688b7318637610
Gregory Losco <gtlosco@gmail.com>
2019-05-08 02:18:36 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -26,4 +26,8 @@ class MusicLibraryController
     Song.all.sort{|a,b| a.name <=> b.name}.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
   
+  def list_artists
+    Artist.all.sort{|a,b| a.name <=> b.name}.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+  
 end
