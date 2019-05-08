de3792d2a6df567453fc378965208ca4e70e833d
Gregory Losco <gtlosco@gmail.com>
2019-05-08 01:52:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     artist, song, genre = file.split(" - ") #more than one variable assigns all to each element
     song_artist = Artist.find_or_create_by_name(artist)
     song_genre = Genre.find_or_create_by_name(genre)
-    new_song = song.new
+    new_song = Song.new(song, song_artist, song_genre)
   end
   
 end
