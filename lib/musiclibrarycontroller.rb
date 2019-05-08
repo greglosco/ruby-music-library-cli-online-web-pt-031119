ec65ac4cdae23e8ba9f285d4dd4e4861e9eef6e6
Gregory Losco <gtlosco@gmail.com>
2019-05-08 02:12:25 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -4,7 +4,7 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
   
-  def call()
+  def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -14,6 +14,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    input = 
   end
   
 end
